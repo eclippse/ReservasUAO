7a4051a9b94288d0c5ae61146ed711a8e02eb945
franks <frank.zabala@uao.edu.co>
2020-06-06 19:33:49 -0500

    Updates

---
+++ precache-manifest.5ac695b0a7e8392c85659f4f082cce1a.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb358f30c17bcb34146ef53a429215fc",
+    "url": "/ReservasUAO/index.html"
+  },
+  {
+    "revision": "ada2f6a438c962eb3f5e",
+    "url": "/ReservasUAO/static/css/main.6f89be93.chunk.css"
+  },
+  {
+    "revision": "f7fc346ffc92e96bf728",
+    "url": "/ReservasUAO/static/js/2.36cb8c56.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/ReservasUAO/static/js/2.36cb8c56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ada2f6a438c962eb3f5e",
+    "url": "/ReservasUAO/static/js/main.095bf2d0.chunk.js"
+  },
+  {
+    "revision": "b1e03e34b56d20e0d333",
+    "url": "/ReservasUAO/static/js/runtime-main.aeb0e3ef.js"
+  },
+  {
+    "revision": "2345adade69e6b669d3e37053ea53f64",
+    "url": "/ReservasUAO/static/media/97490558_3423786350998934_5278027736017797120_o.2345adad.png"
+  },
+  {
+    "revision": "5bf0c0e8ad67fe6a2cac8529bc40a674",
+    "url": "/ReservasUAO/static/media/98206597_3457902510920651_9156930404614144000_o.5bf0c0e8.png"
+  },
+  {
+    "revision": "dabacba220dce62ba1ba941ea3f2a424",
+    "url": "/ReservasUAO/static/media/98447653_3452224514821784_595117677920387072_o.dabacba2.png"
+  },
+  {
+    "revision": "50df7bf8e5b72885fc9a2c947b5a337a",
+    "url": "/ReservasUAO/static/media/98807578_3453338404710395_6862215621304647680_o.50df7bf8.jpg"
+  },
+  {
+    "revision": "45b139c18aa47571c35af31af223b193",
+    "url": "/ReservasUAO/static/media/99292859_3465365123507723_6752478790280019968_o.45b139c1.jpg"
+  },
+  {
+    "revision": "56391028d464d78156fd6f39942b1a6c",
+    "url": "/ReservasUAO/static/media/LogoA.56391028.png"
+  },
+  {
+    "revision": "46cc2ddcae30e54bf44e1293b07fa34c",
+    "url": "/ReservasUAO/static/media/imagen-login.46cc2ddc.png"
+  },
+  {
+    "revision": "b7be6840747858c8a77e572970195749",
+    "url": "/ReservasUAO/static/media/imgUser.b7be6840.png"
+  },
+  {
+    "revision": "30d93a18b4df15f7f2c9cddc329befd5",
+    "url": "/ReservasUAO/static/media/logoN.30d93a18.png"
+  },
+  {
+    "revision": "8dd81963f84f604bf0739cc2349110d5",
+    "url": "/ReservasUAO/static/media/logo_Horizontal.8dd81963.png"
+  },
+  {
+    "revision": "de1d141c570d00b45a42ca6887680fe0",
+    "url": "/ReservasUAO/static/media/mapa.de1d141c.png"
+  }
+]);
